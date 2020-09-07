df1b3ca14540d8c9185891435e486baaab1735d6
Aleksei <amashuroff@gmail.com>
2020-09-07 14:19:22 +0300

    practicing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
 // import React and ReactDOM libs
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 // create React component
-const App = () => {
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-  const btnText = 'click me';
-  const style = { backgroundColor: "green", color: "red" };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: "Failed to get the position" })
+    );
+  }
 
-  return (
-    <div>
-      <label className="label" for="name">
-        Enter name
-      </label>
-      <input id="name" />
-      <button style={style}>
-        {btnText}
-      </button>
-    </div>
-  );
-};
+  helper() {
+    if (this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    } else {
+      return <Spinner message="Please accept location request" />;
+    }
+  }
+
+  render() {
+    return <div>{this.helper()}</div>;
+  }
+}
 
 // take React component, show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
